6d7e88839cc23b92148228bf5215595079cbc8ff
Damian Soto <soto.platero@gmail.com>
2020-11-24 06:02:46 -0500

    add form

--- components/BtnCreate.vue
+++ components/BtnCreate.vue
@@ -1,6 +1,5 @@
 <template>
     <button  
-    	@click.prevent="create"
 	    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-lg font-bold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full">
       Publicar
     </button>    	
